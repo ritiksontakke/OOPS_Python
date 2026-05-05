07ff62791d63e6d83192f1608e680e50cc7fca3a
Ritik Arun Sontakke <ritiksontakke1008@gmail.com>
2026-05-05 18:17:24 +0530

    pratice que

--- inheritance/1st_problem.py
+++ inheritance/1st_problem.py
@@ -36,4 +36,46 @@
 #   print(cat)         # Cat: Whiskers, Age: 2, Color: orange
 # ============================================================
 
+class Animal :
 
+    def __init__(self, name , age):
+        self.name = name
+        self.age = age
+    
+    def eat(self):
+        print(f"{self.name} is eating")
+    
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+    
+    def __str__(self):
+        return f"Animal : {self.name}, Age : {self.age}"
+    
+class Dog(Animal):
+    def __init__(self, name, age, breed):
+        super().__init__(name, age)
+        self.breed = breed
+    
+    def fetch(self, item):
+        print(f"{self.name} fetched the {item}!")
+
+    def __str__(self):
+        return f"Dog : {self.name}, Age: {self.age}, breed : {self.breed}"
+    
+class Cat(Animal):
+    def __init__(self, name, age, color):
+        super().__init__(name, age)  # Call parent constructor
+        self.color = color
+
+    def purr(self):
+        print(f"{self.name} is purring...")
+
+    def __str__(self):
+        return f"Cat: {self.name}, Age: {self.age}, Color: {self.color}"
+    
+dog = Dog("Bruno", 3, "Labrador")
+dog.eat()          # Bruno is eating.
+dog.fetch("ball")  # Bruno fetched the ball!
+print(dog)         # Dog: Bruno, Age: 3, Breed: Labrador
+
+print()  # Just for spacing
